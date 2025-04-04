5b6bd370c3da45603022856841dce0e5be07937c
Hp <abdullahalnoman1512@gmail.com>
2025-04-04 17:41:45 +0600

    added

--- src/shared/catchAsync.ts
+++ src/shared/catchAsync.ts
@@ -1,15 +1,27 @@
 import { Request,Response,NextFunction, RequestHandler } from "express"
 
-const catchAsync = (fn:RequestHandler) => {
+const catchAsync = (fn: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  };
+};
 
-    return async (req:Request,res:Response,next:NextFunction) => {
-        try {
-            fn(req,res,next)
-          } catch (error) {
-            next(error);
-          }
 
-    }
-}
+// const catchAsync = (fn:RequestHandler) => {
+
+//     return async (req:Request,res:Response,next:NextFunction) => {
+//         try {
+//             fn(req,res,next)
+//           } catch (error) {
+//             console.log('error - hoice',error)
+//             next(error);
+//           }
+
+//     }
+// }
+
+// export default catchAsync;
+
+
 
 export default catchAsync;
